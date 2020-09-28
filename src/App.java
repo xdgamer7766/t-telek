fab829bec4beece9c9404753d05976badbfdc7a7
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-09-28 06:53:20 +0200

    beolvasás

--- src/App.java
+++ src/App.java
@@ -1,12 +1,23 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class App {
-    public static String[] szavak; 
+    public static String[] words;
+
     public static void main(String[] args) throws Exception {
         f0();
     }
 
 
     public static void f0() throws Exception{
-
+        File f = new File("szavak.txt");
+        Scanner scr = new Scanner(f);
+        int i = 0;
+        while (scr.hasNextLine()){
+            words[i] = scr.nextLine();
+            i++;
+        }
+        scr.close();  
     }
 
 
