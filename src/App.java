843e22561afab50bb62a5182e9f3715e5bc16500
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-09-28 07:39:57 +0200

    eldöntés és kiválasztás

--- src/App.java
+++ src/App.java
@@ -1,23 +1,25 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
     public static String[] words;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws FileNotFoundException {
         f0();
         f1();
+        f2();
+        f3();
     }
 
-
-    public static void f0() throws Exception{
+    public static void f0() throws FileNotFoundException {
         File f = new File("szavak.txt");
         Scanner scr = new Scanner(f);
         int i = 0;
-        while (scr.hasNextLine()){
-            words[i] = scr.nextLine();
-            i++;
-        }
+            while (scr.hasNextLine()){
+                words[i] = scr.nextLine();
+                i++;
+            }
         scr.close();  
     }
 
@@ -32,6 +34,38 @@ public class App {
             ascii =ascii + (int) onechar;
         }
         System.out.println(ascii);
+    }
+
+
+    public static void f2(){
+        int ewrd=0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].charAt(1) == 'e'){
+                ewrd++;
+            }
+        }
+        System.out.println(ewrd);
+    }
+
+
+    public static void f3(){
+        boolean a=true;
+        int i=0;
+        String wrd;
+        while(a){
+            wrd = words[i];
+            if(wrd == "ostor") {
+                System.out.println("Az ostor szó szerepel a fájlban \n Hányadik elem a fájlban :");
+                System.out.println(i);
+                a=false;
+            }
+            if(wrd == "ostoros") {
+                System.out.println("Az ostor szó szerepel a fájlban \n Hányadik elem a fájlban :");
+                System.out.println(i);
+                a=false;
+            }
+            i++;
+        }
     }
 
 
