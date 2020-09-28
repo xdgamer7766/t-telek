476c359fc3d4c334226a15d0a8334bce7876bdec
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-09-28 06:38:48 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static String[] szavak; 
+    public static void main(String[] args) throws Exception {
+        f0();
+    }
+
+
+    public static void f0() throws Exception{
+
+    }
+
+
+}
